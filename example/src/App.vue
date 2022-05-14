e83d65acb3e227a8153d15849b88512557dfac0b
Orest <orest-2@yandex.ua>
2022-05-14 09:57:36 +0300

    upd example

--- example/src/App.vue
+++ example/src/App.vue
@@ -9,9 +9,8 @@
     >
       <swiper-slide
         v-for="n in 5"
-        :key="n"
+        :key="29+n"
         class="test"
-        :class="{test_2: true}"
       >
         <div>{{ 29+n }}</div>
         <img
@@ -28,14 +27,14 @@
       :slides-per-view="3"
       :space-between="30"
       loop
+      autoplay
       @swiper="onSwiper"
       @slideChange="onSlideChange"
     >
       <swiper-slide
         v-for="n in 5"
-        :key="n"
+        :key="34+n"
         class="test"
-        :class="{test_2: true}"
       >
         <div>{{ 34+n }}</div>
         <img
@@ -62,7 +61,6 @@
         v-for="n in 5"
         :key="n"
         class="test"
-        :class="{test_2: true}"
       >
         <div>{{ 34+n }}</div>
         <img
@@ -74,24 +72,71 @@
         >
       </swiper-slide>
     </swiper>
+
+    <h2>Movies</h2>
+    <swiper
+      :slides-per-view="4"
+      loop
+      autoplay
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide
+        v-for="film in movies"
+        :key="film.id"
+      >
+        <div
+          class="card"
+          :style="{ 'background-image': 'url(' + getImageUrl(film.id) + ')' }"
+        >
+          <h1>
+            {{ film.title }}
+          </h1>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { Navigation, Pagination } from 'swiper'
+import { Navigation, Pagination, Autoplay } from 'swiper'
 import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
+
+// import { SwiperCore, Swiper, SwiperSlide } from '../../src/index.js'
 
 // Import Swiper styles
 import 'swiper/swiper-bundle.css'
 
-SwiperCore.use([Navigation, Pagination])
+SwiperCore.use([Navigation, Pagination, Autoplay])
 
 export default Vue.extend({
   name: 'App',
   components: {
     Swiper,
     SwiperSlide
+  },
+  computed: {
+    movies () {
+      return [
+        {
+          id: 34,
+          title: 'Film 1'
+        },
+        {
+          id: 35,
+          title: 'Film 2'
+        },
+        {
+          id: 36,
+          title: 'Film 3'
+        },
+        {
+          id: 37,
+          title: 'Film 3'
+        }
+      ]
+    }
   },
   methods: {
     getImageUrl (imageId: any) {
@@ -120,5 +165,13 @@ export default Vue.extend({
 }
 .mr-auto, .mx-auto {
   margin-right: auto;
+}
+.card {
+  width: 400px;
+  height: 400px;
+  margin: auto;
+  h1 {
+    color: #fff;
+  }
 }
 </style>
